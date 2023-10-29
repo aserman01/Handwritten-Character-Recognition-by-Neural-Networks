127a00ec43b8ba24a65a86b51ecab168bd95bb42
Anıl Tan Aktan <aniltanaktan@gmail.com>
2023-10-29 14:25:47 +0300

    Add files via upload

--- Project 1 - Number Recognition.py
+++ Project 1 - Number Recognition.py
@@ -6,10 +6,10 @@ Created on Fri Oct 27 10:37:22 2023
 @author: aniltanaktan
 """
 
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm 
 import numpy as np
-import math
-import pandas as pd
+
 
 
 # Import data from local folder. x = data, y = labels
@@ -89,6 +89,21 @@ m, n = test_l.shape
 print(m,n)
 """
 
+"""
+# Visualizing the training data and see how frequent each digit occured
+index = 1049
+plt.title((train_l[index]))
+plt.imshow(train[index].reshape(28,28), cmap=cm.binary)
+
+print("Train Data Frequencies:")
+y_value=np.zeros((1,10))
+for i in range (10):
+    print(i,"occurances: ",np.count_nonzero(train_l==i))
+    y_value[0,i-1]= np.count_nonzero(train_l==i)
+
+"""
+
+
 # Now we have 2 arrays, train is 60000x784 matrix, contains each data, 
 #                   and train_l is 60000x1 matrix, contains each label
 # and we have 2 arrays, test is 10000x784 matrix, contains each data, 
@@ -99,9 +114,9 @@ print(m,n)
 # activation function as tanh and RELU
 # learning coefficient = 0.01, 0.05, 0.09
 
-# Initializing weights, needed functions
+# Initializing weights, b0 (bias) and needed activation functions
 
-def initialize(N):
+def initialize(N): # Weights have -0.01 : 0.01 uniform dist.
     W1 = np.random.uniform(-0.01, 0.01, (N, 784))
     b1 = np.random.uniform(-0.01, 0.01, (N, 1))
     W2 = np.random.uniform(-0.01, 0.01, (N, 784))
@@ -125,6 +140,37 @@ def d_tanh(v):
     a = (np.exp(v)-np.exp(-v))/(np.exp(v)+np.exp(-v))
     return 1-a**2
 
+# Functions for each case 3x3x2 = 18
 
-print(initialize(300)[0].shape)
+# For Case 1, y will have 1 for true class, and -1 for others
+
+def one_min_one(y):
+    one_min_one_y = np.zeros((y.size, y.max() + 1)) - 1
+    one_min_one_y[np.arrange(y.size), y] = 1 # each row of the minus one vector which corresponds to classified number will become 1
+    # for example: if classification = 5, 5th elementh of zeroes vector will become 1, others will stay as -1
+    one_min_one_y = one_min_one_y.T
+    return one_min_one_y
+    
+
+# For Case 2, y will have 1 for true class, and 0 for others
+
+def one_zero(y):
+    one_zero_y = np.zeros((y.size, y.max() + 1))
+    one_zero_y[np.arrange(y.size), y] = 1 # each row of the zeroes vector which corresponds to classified number will become 1
+    # for example: if classification = 5, 5th elementh of zeroes vector will become 1
+    one_zero_y = one_zero_y.T
+    return one_zero_y
+    
+def forward_propagation(v1, o1, v2, o2, W2, y, activation):
+    
+    return
+
+def back_propagation(v1, o1, v2, o2, W2, y, activation):
+    
+    return
+
+
+
+
+
 
