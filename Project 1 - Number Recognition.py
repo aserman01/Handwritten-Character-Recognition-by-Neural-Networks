097634cb873eb9c9be3bb9a74e615bcc0b30c619
Anıl Tan Aktan <aniltanaktan@gmail.com>
2023-10-30 13:57:33 +0300

    Add files via upload

--- Project 1 - Number Recognition.py
+++ Project 1 - Number Recognition.py
@@ -67,8 +67,8 @@ test_y.close()
 
 
 
-train= np.array(train)
-train_l= np.array(train_l)
+train= np.array(train).T
+train_l= np.array(train_l).T
 
 """
 m, n = train.shape
@@ -78,8 +78,8 @@ m, n = train_l.shape
 print(m,n)
 """
 
-test= np.array(test)
-test_l= np.array(test_l)
+test= np.array(test).T
+test_l= np.array(test_l).T
 
 """
 m, n = test.shape
@@ -119,20 +119,24 @@ for i in range (10):
 def initialize(N): # Weights have -0.01 : 0.01 uniform dist.
     W1 = np.random.uniform(-0.01, 0.01, (N, 784))
     b1 = np.random.uniform(-0.01, 0.01, (N, 1))
-    W2 = np.random.uniform(-0.01, 0.01, (N, 784))
-    b2 = np.random.uniform(-0.01, 0.01, (N, 1))
+    W2 = np.random.uniform(-0.01, 0.01, (10, N))
+    b2 = np.random.uniform(-0.01, 0.01, (10, 1))
     return W1, b1, W2, b2
 
 def RELU(v):
     return np.maximum(0, v)
 
 def d_RELU(v):
+    
+    return v>0
+    """
     if v > 0:
         return 1
     
     else:
         return 0
-
+    """
+    
 def tanh(v):
     return (np.exp(v)-np.exp(-v))/(np.exp(v)+np.exp(-v))
 
@@ -156,18 +160,94 @@ def one_min_one(y):
 
 def one_zero(y):
     one_zero_y = np.zeros((y.size, y.max() + 1))
-    one_zero_y[np.arrange(y.size), y] = 1 # each row of the zeroes vector which corresponds to classified number will become 1
+    one_zero_y[np.arange(y.size), y] = 1 # each row of the zeroes vector which corresponds to classified number will become 1
     # for example: if classification = 5, 5th elementh of zeroes vector will become 1
     one_zero_y = one_zero_y.T
     return one_zero_y
-    
-def forward_propagation(v1, o1, v2, o2, W2, y, activation):
-    
-    return
 
-def back_propagation(v1, o1, v2, o2, W2, y, activation):
+
+# Z = v, A = o
+def forward_propagation(W1, b1, W2, b2, X, activation):
     
-    return
+    v1 = W1.dot(X)+b1
+
+    o1 = RELU(v1)
+    
+    v2 = W2.dot(o1)+b2
+
+    o2 = tanh(v2)
+    
+    return v1, o1, o2, v2
+
+def back_propagation(v1, o1, v2, o2, W2, x, y, activation):
+    m = y.size
+    one_zero_y = one_zero(y)
+    
+    dv2 = o2 - one_zero_y
+    dW2 = (1/m) * dv2.dot(o1.T)
+    db2 = (1/m) * np.sum(dv2, axis=1).reshape(-1,1)
+    
+    dv1 = W2.T.dot(dv2) * d_RELU(v1)
+    dW1 = (1/m) * dv1.dot(x.T)
+    db1 = (1/m) * np.sum(dv1, axis=1).reshape(-1,1)
+    
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, l_rate):
+    W1 = W1 - l_rate*dW1
+    b1 = b1 - l_rate*db1
+    W2 = W2 - l_rate*dW2
+    b2 = b2 - l_rate*db2
+    
+    return W1, b1, W2, b2
+
+def get_predictions(o2):
+    return np.argmax(o2, 0)
+
+def get_accuracy(predictions, y):
+    print(predictions, y)
+    return np.sum(predictions == y) / y.size
+
+def gradient_descent(x, y, epochs, l_rate, N):
+    W1, b1, W2, b2 = initialize(N)
+    
+    for i in range(epochs):
+        v1, o1, v2, o2 = forward_propagation(W1, b1, W2, b2, x, "relu")
+        dW1, db1, dW2, db2 = back_propagation(v1, o1, v2, o2, W2, x, y, "relu")
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, l_rate)
+        
+        if i % 25 == 0:
+            print("Epoch: ", i)
+            print("Accuracy: ", get_accuracy(get_predictions(o2), y))
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(train, train_l, 500, 0.01, 300)
+print("W1: ",W1,"b1: ", b1,"W2: ", W2,"b2: ", b2)
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_propagation(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = train[:, index, None]
+    prediction = make_predictions(train[:, index, None], W1, b1, W2, b2)
+    label = train_l[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+test_prediction(0, W1, b1, W2, b2)
+test_prediction(1, W1, b1, W2, b2)
+test_prediction(2, W1, b1, W2, b2)
+test_prediction(3, W1, b1, W2, b2)
+
+dev_predictions = make_predictions(test, W1, b1, W2, b2)
+get_accuracy(dev_predictions, test_l)
 
 
 
